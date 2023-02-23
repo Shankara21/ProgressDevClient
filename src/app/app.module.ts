b094f3bd5795619663fe568db25f566d90eb214c
Shankara21 <lazuardit21@gmail.com>
2023-02-23 10:08:49 +0700

    Initialize Project ProgressDev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { FooterComponent } from './layouts/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    DashboardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/layouts/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  host: { class: 'flex justify-center' },
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+}
